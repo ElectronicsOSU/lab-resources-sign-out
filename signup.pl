7a17483f15edaea1b0878558ee4040610fae9491
Eli Gladman <eli.gladman@gmail.com>
2015-09-09 19:07:48 -0400

    modified for lab resources

--- signup.pl
+++ signup.pl
@@ -7,6 +7,11 @@ system('clear');
 
 my $line;
 
+
+
+
+
+
 print "Enter your last name.#: ";
 
 while (<>) {
@@ -15,14 +20,38 @@ while (<>) {
 
     if ($name =~ m/\A\w+\.\d+\z/xms) {
         open(NAMES, '>>names.txt');
-        print NAMES $name, '@osu.edu', "\n";
+        print NAMES $name, '@osu.edu';
         close NAMES;
 
+	# system('clear');
+  #       print "Okay $name, thanks for signing up!\n";
+  #   } else {
+  #       print "Sorry, try again!\n";
+  #   }
+  #
+  #   print "Enter your name.#: ";
+}
+
+
+
+
+print "Enter the Inventory Number: ";
+
+while (<>) {
+    my $num = $_;
+    chomp($num);
+
+    if ($num =~ m/\d+\z/xms) {
+        open(NUM, '>>names.txt');
+        print NUM " | part number: ", $num, "\n";
+        close NUM;
+
 	system('clear');
-        print "Okay $name, thanks for signing up!\n";
+        print "Okay $name, thanks!\n";
     } else {
         print "Sorry, try again!\n";
     }
 
     print "Enter your name.#: ";
+}
 }
