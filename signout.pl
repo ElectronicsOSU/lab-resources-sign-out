9dd23cc7fc8c879471b8c88a3cd6ece11c41e71c
Eli Gladman <eli.gladman@gmail.com>
2015-09-09 21:20:48 -0400

    Update signout.pl

--- signout.pl
+++ signout.pl
@@ -5,13 +5,6 @@ use strict;
 
 system('clear');
 
-my $line;
-
-
-
-
-
-
 print "Enter your last name.#: ";
 
 while (<>) {
@@ -19,39 +12,25 @@ while (<>) {
     chomp($name);
 
     if ($name =~ m/\A\w+\.\d+\z/xms) {
-        open(NAMES, '>>names.txt');
-        print NAMES $name, '@osu.edu';
-        close NAMES;
 
-	# system('clear');
-  #       print "Okay $name, thanks for signing up!\n";
-  #   } else {
-  #       print "Sorry, try again!\n";
-  #   }
-  #
-  #   print "Enter your name.#: ";
-}
+      print "Enter the Inventory Number: ";
+      my $num = <STDIN>;
+      chomp($num);
 
+        if ($num =~ m/\d+\z/xms) {
+            open(NAMES, '>>names.txt');
+            print NAMES $name, '@osu.edu', " | Inventory #: ", $num, "\n";
+            close NAMES;
 
+            system('clear');
+            print "Thanks";
 
+          } else {
+            print "Sorry, try again. Are you sure you entered a valid integer?\n";
+            }
+          } else {
+          print "Sorry, try again!\n";
+        }
 
-print "Enter the Inventory Number: ";
-
-while (<>) {
-    my $num = $_;
-    chomp($num);
-
-    if ($num =~ m/\d+\z/xms) {
-        open(NUM, '>>names.txt');
-        print NUM " | part number: ", $num, "\n";
-        close NUM;
-
-	system('clear');
-        print "Okay $name, thanks!\n";
-    } else {
-        print "Sorry, try again!\n";
-    }
-
-    print "Enter your name.#: ";
-}
-}
+    print "Enter your last name.#: ";
+  }
